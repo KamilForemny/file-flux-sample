a048164853eeec32fbfb3e40c5a7988a172f9216
kamil <kamilforemny@gmail.com>
2018-11-25 12:08:50 +0100

    some clean up

--- src/main/kotlin/com/samples/fileflux/controller/FileController.kt
+++ src/main/kotlin/com/samples/fileflux/controller/FileController.kt
@@ -6,18 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.Duration
 
+const val FILES_MAPPING = "/files"
+const val IMAGES_PATH = "package/images"
 
 @RestController
 @RequestMapping
 class FileController {
 
-    @GetMapping("/files", produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
-    fun getFilesAsFlux(): Flux<Path> {
-        return Flux.fromStream(Files.list((Paths.get("package/images")))).delayElements(Duration.ofSeconds(1L))
-    }
+    @GetMapping(FILES_MAPPING, produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    fun getFilesAsFlux() = Flux
+            .fromStream(Files.list((Paths.get(IMAGES_PATH))))
+            .delayElements(Duration.ofSeconds(1L))
 
 }
